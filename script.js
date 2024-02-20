41c588ba925ca9691eb19465f8b06136d136d28b
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-20 10:05:44 +0530

    capture all the element first

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const screens = document.querySelector('.screen')
+const screens = document.querySelector('.screen');
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn')
+const game_container = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
 
-screens
+let seconds = 10
+let score = 0
+let selected_insect = {}
+
+
+
+
