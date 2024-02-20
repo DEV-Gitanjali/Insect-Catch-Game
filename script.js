d2a57b4d0f023e157159f103fdd9af1c5279ba80
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-20 10:54:15 +0530

    this method for catch the insect

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ start_btn.addEventListener('click' , ()=>
 )
 
 
+choose_insect_btns.forEach(btn =>{
+    btn.addEventListener('click' , ()=>{
+
+        const img = btn.querySelector('img')
+        const src = btn.querySelector('src')
+        const alt = btn.querySelector('alt')
+        selected_insect ={src,alt}
+        screens[1].classList.add('up')
+
+        setTimeout(createInsect,1000)
+        StartGame()
+
+        
+    })
+})
+
+function createInsect(){
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    const {x,y} = getRandomLocation()
+
+}
+
